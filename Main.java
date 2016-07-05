3a7253c805f3cbceeb99cdfaf87d1a0223a134e7
RamIowaStateUniversity <rramu@iastate.edu>
2016-07-05 15:38:20 -0500

    4

--- Main.java
+++ Main.java
@@ -1,23 +1,15 @@
 import java.util.Scanner;
 
+enum UserStatus {
+    PENDING,
+    ACTIVE,
+    INACTIVE,
+    DELETED;
+}
+
 public class Main {
-	private static final String getGeneratedClass(final CommandLine cl) {
-		// get the name of the generated class
-		final String className;
-		if (cl.hasOption('n')) {
-			className = cl.getOptionValue('n');
-		} else {
-			String s = "";
-			for (final File f : inputFiles) {
-				if (s.length() != 0)
-					s += "_";
-				if (f.getName().indexOf('.') != -1)
-					s += f.getName().substring(0, f.getName().lastIndexOf('.'));
-				else
-					s += f.getName();
-			}
-			className = pascalCase(s);
-		}
-		return className;
+	private void getGeneratedClass(String cl) {
+		int i=0;
+		System.out.println(UserStatus.ACTIVE);
 	}
 }
